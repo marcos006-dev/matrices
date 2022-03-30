26076d061f2fbd3bbf27473acebfed3901cda9e8
Marcos Franco <marcosfrc013@gmail.com>
2022-03-30 15:28:09 -0300

    Se finalizo el ejercicio

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const randomInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-function fill2DimensionsArray(rows, columns) {
+const fill2DimensionsArray = (rows, columns) => {
   const arr = [];
   for (let i = 0; i < rows; i++) {
     arr.push([0]);
     for (let j = 0; j < columns; j++) {
-      arr[i][j] = randomInteger(1, 5);
+      arr[i][j] = randomInteger(1, 100);
     }
   }
 
   return arr;
-}
-// matriz 1
-const rows = randomInteger(1, 2);
-const columns = randomInteger(1, 2);
+};
+const rows = randomInteger(1, 10);
+const columns = randomInteger(1, 10);
 
 const matriz1 = fill2DimensionsArray(rows, columns);
 const matriz2 = fill2DimensionsArray(columns, rows);
@@ -23,37 +22,21 @@ console.table(matriz1);
 console.table('------------------------------------------------');
 console.table(matriz2);
 
-const matriz3 = [];
+let matriz3 = [0];
 
-let suma;
-
-// console.log(matriz1.length);
 for (let i = 0; i < matriz1.length; i++) {
-  suma = 0;
-  for (let j = 0; j < matriz2.length; j++) {
-    //   for (let z = 0; z < matriz1[i].length; z++) {
-    // }
-    // console.log(matriz1[i]);
-    // console.log(matriz2[j][i]);
-
-    for (let z = 0; z < matriz2[j].length; z++) {
-      // suma = matriz1[i][z] * matriz2[j][z];
-      console.log(matriz2[j][z] * matriz1[i][z]);
+  matriz3[i] = [0];
+  // recorrer las filas de la matriz 2
+  for (let j = 0; j < matriz2[0].length; j++) {
+    matriz3[i][j] = 0;
+    // multiplicar cada fila de la matriz 1 por cada columna de la matriz 2
+    for (let k = 0; k < matriz1[0].length; k++) {
+      // console.log(
+      //   `${matriz1[i][k]} * ${matriz2[k][j]} = ${matriz1[i][k] * matriz2[k][j]}`
+      // );
+      matriz3[i][j] += matriz1[i][k] * matriz2[k][j];
     }
-
-    // console.log(suma);
   }
-  //   console.log(
-  //     `${matriz1[i][j]} * ${matriz2[j][i]} = ${matriz1[i][j] * matriz2[j][i]}`
-  //   );
-  //   // suma += matriz1[i][j] * matriz2[j][i];
-  //   console.log(matriz1[i][j] * matriz2[j][i]);
-  // matriz3.push(suma);
-
-  // suma = 0;
-  // console.log(matriz3);
 }
 
-// console.table('------------------------------------------------');
-
-// console.table(matri3);
+console.table(matriz3);
